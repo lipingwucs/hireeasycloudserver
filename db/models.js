dae20d889ffc7172b9a76fd8c182b28f3f742512
Liping Wu <lipingwucs@gmail.com>
2021-04-15 12:07:23 -0400

    Database URL log added

--- db/models.js
+++ db/models.js
@@ -5,6 +5,7 @@ Contains some Model modules that manipulate database collection data
 /*Connect to the database*/
 const mongoose = require('mongoose')
 const databaseURI = databaseUri || 'mongodb://localhost:27017/hireeasy_db'
+console.log("DataBase URL", databaseURI);
 mongoose.connect(databaseURI) 
 
 const conn = mongoose.connection
